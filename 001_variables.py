e4a189f150572e5881baea4544470d2b7eb97c26
BryanRomoCode <91690651+BryanRomoCode@users.noreply.github.com>
2022-08-18 19:44:00 -0500

    Add files via upload

---
+++ 001_variables.py
@@ -0,0 +1,17 @@
+"""
+Variables
+
+Practica 1:
+
+Escribe un mensaje en una variable que puedes llamar como quieras.
+Crea dos variables y almacena en ellas un número.
+Intenta (todavía no te he mostrado como) sumar las dos variables del ejercicio anterior y guarda el resultado de esta suma en una tercera variable.
+Si has conseguido hacer el ejercicio anterior, imprime el valor de la variable del resultado.
+En cambio, si no lo has conseguido imprime el texto de la variable del primer ejercicio. 
+"""
+
+variable1 = "Hola" #Variable de tipo string
+variable1= 1 #Cambiar la variable de tipo string a int
+variable2 = 2 #Declarar segunda variable de valor string
+
+print(variable1+variable2) #Sumar las variables de tipo int
