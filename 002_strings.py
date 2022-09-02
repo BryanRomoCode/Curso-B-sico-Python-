6c7cf37ac1347b53743d8e4485d1fde81eae9ac3
Bryan Romo <91690651+BryanRomoCode@users.noreply.github.com>
2022-09-01 20:00:05 -0500

    Add files via upload

---
+++ 002_strings.py
@@ -0,0 +1,7 @@
+"""Escribe dos variables con strings. Uno con comillas simples '' y otro con comillas dobles"". """
+
+variable1 = "Esta es la variable con comillas dobles"
+variable2 = 'Esta es la variable con comillas simples'
+
+print(variable1)
+print(variable2)
