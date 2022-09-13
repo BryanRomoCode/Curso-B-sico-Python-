d8fe5f00f4eaa92b912f5ea7b5434b0c9b647a78
Bryan Romo <91690651+BryanRomoCode@users.noreply.github.com>
2022-09-13 10:50:57 -0500

    Add files via upload

---
+++ 007_operadores.py
@@ -0,0 +1,16 @@
+'''
+Almacena en una variable una suma que de como resultado el número 87. Es obligatorio incluir en dicha suma los números 20 y 23.
+Almacena en una variable una resta que de como resultado el número negativo -87. Es obligatorio incluir en dicha resta los números positivos 20 y 23.
+Almacena en una variable una multiplicación que de como resultado el número 870. Es obligatorio incluir en dicha multiplicación los números 20 y 23.'''
+
+suma = 20 + 23 + 44
+
+print(suma)
+
+resta = (20 + 23) - 130
+
+print (resta)
+
+mult = (20+23) * 20.23255813959
+
+print(mult)
